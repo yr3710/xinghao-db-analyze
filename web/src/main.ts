b8b8fe29dcd35209ae45e1f50d250d78623a4500
yirui <3167228656@qq.com>
2026-07-30 00:23:11 +0800

    fix-加入 UnoCSS

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,10 +3,9 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import { setupRouter } from '@/router'
 import { setupStore } from '@/store'
-
+import 'virtual:uno.css'
 
 const app = createApp(App)
-
 
 async function setupApp() {
   setupStore(app)

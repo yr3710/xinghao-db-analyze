f4dea3c65c47b27b8b5272750f1a6b22fed48d14
yirui <3167228656@qq.com>
2026-07-29 16:55:22 +0800

    add-参数自动解析并添加参数解析测试接口

--- controllers/health_api.py
+++ controllers/health_api.py
@@ -1,5 +1,7 @@
+from pydantic import BaseModel
 from sanic import Blueprint, Request
 
+from common.param_parser import parse_params
 from common.res_decorator import async_json_resp
 
 
@@ -9,10 +11,38 @@ bp = Blueprint(
 )
 
 
+class EchoRequest(BaseModel):
+    query: str
+    count: int = 1
+
+
 @bp.get("/health")
 @async_json_resp
 async def health(request: Request):
     return {
         "name": "xinghao-db-analyze",
         "status": "running",
+    }
+
+
+@bp.post("/echo")
+@async_json_resp
+@parse_params
+async def echo(
+    request: Request,
+    body: EchoRequest,
+):
+    return body.model_dump()
+
+
+@bp.get("/add")
+@async_json_resp
+@parse_params
+async def add(
+    request: Request,
+    number_a: int,
+    number_b: int,
+):
+    return {
+        "result": number_a + number_b,
     }

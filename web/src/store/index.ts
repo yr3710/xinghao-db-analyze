194300a05b73997bb3d0868d1e676db6538ab2b6
yirui <3167228656@qq.com>
2026-08-03 10:30:46 +0800

    add-完善pinia基础设施

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,13 +1,11 @@
-import type { App } from 'vue'
 import { createPinia } from 'pinia'
-
+import { pluginPinia } from '@/store/plugins'
 
 const store = createPinia()
 
-
-export function setupStore(app: App) {
+export function setupStore(app: App<Element>) {
   app.use(store)
 }
 
-
+store.use(pluginPinia)
 export { store }
